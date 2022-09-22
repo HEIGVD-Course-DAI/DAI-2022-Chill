fbc2231c6b36a6e160706c36cefc1c5d87105890
SNSantos <dedale.sigma@gmail.com>
2022-09-22 11:52:03 +0200

    copy the modele from wasagi to SNSabtos

---
+++ src/main/java/ch/heigvd/dai/chill/domain/SNSantos/IProduct.java
@@ -0,0 +1,10 @@
+package ch.heigvd.dai.chill.domain.SNSantos;
+
+import java.math.BigDecimal;
+
+public interface IProduct {
+
+  String getName();
+  BigDecimal getPrice();
+
+}
---
+++ src/test/java/ch/heigvd/dai/chill/domain/SNSantos/PunkIPATest.java
@@ -0,0 +1,32 @@
+package ch.heigvd.dai.chill.domain.SNSantos;
+
+import ch.heigvd.dai.chill.domain.Bartender;
+import ch.heigvd.dai.chill.domain.wasadigi.PunkIPA;
+import ch.heigvd.dai.chill.protocol.OrderRequest;
+import ch.heigvd.dai.chill.protocol.OrderResponse;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PunkIPATest {
+
+  @Test
+  void thePriceAndNameForPunkIPAShouldBeCorrect() {
+    PunkIPA beer = new PunkIPA();
+    assertEquals(beer.getName(), PunkIPA.NAME);
+    assertEquals(beer.getPrice(), PunkIPA.PRICE);
+  }
+
+  @Test
+  void aBartenderShouldAcceptAnOrderForPunkIPA() {
+    Bartender jane = new Bartender();
+    String productName = "ch.heigvd.dai.chill.domain.wasadigi.PunkIPA";
+    OrderRequest request = new OrderRequest(3, productName);
+    OrderResponse response = jane.order(request);
+    BigDecimal expectedTotalPrice = PunkIPA.PRICE.multiply(new BigDecimal(3));
+    assertEquals(expectedTotalPrice, response.getTotalPrice());
+  }
+
+}
