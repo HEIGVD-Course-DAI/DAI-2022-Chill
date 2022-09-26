7acb18b04c7bde628c64c13f9dd400d5c35a6c3c
Pillonel-on-github <bastien.pil01@gmail.com>
2022-09-26 16:14:14 +0200

    Add: Fin du monde beer
    #9

---
+++ src/test/java/ch/heigvd/dai/chill/domain/wasadigi/FinDuMondeTest.java
@@ -0,0 +1,31 @@
+package ch.heigvd.dai.chill.domain.wasadigi;
+
+import ch.heigvd.dai.chill.domain.Bartender;
+import ch.heigvd.dai.chill.protocol.OrderRequest;
+import ch.heigvd.dai.chill.protocol.OrderResponse;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FinDuMondeTest {
+
+  @Test
+  void thePriceAndNameForFinDuMondeShouldBeCorrect() {
+    FinDuMonde beer = new FinDuMonde();
+    assertEquals(beer.getName(), FinDuMonde.NAME);
+    assertEquals(beer.getPrice(), FinDuMonde.PRICE);
+  }
+
+  @Test
+  void aBartenderShouldAcceptAnOrderForFinDuMonde() {
+    Bartender jane = new Bartender();
+    String productName = "ch.heigvd.dai.chill.domain.wasadigi.FinDuMonde";
+    OrderRequest request = new OrderRequest(3, productName);
+    OrderResponse response = jane.order(request);
+    BigDecimal expectedTotalPrice = FinDuMonde.PRICE.multiply(new BigDecimal(3));
+    assertEquals(expectedTotalPrice, response.getTotalPrice());
+  }
+
+}
