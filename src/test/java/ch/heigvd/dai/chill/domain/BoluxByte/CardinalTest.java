c58e9ea66e4f625d7785c3c4366316d5fd6f9457
Felix Breval <felix.breval@heig-vd.ch>
2022-09-29 10:18:13 +0200

    added cardinalTest

---
+++ src/test/java/ch/heigvd/dai/chill/domain/BoluxByte/CardinalTest.java
@@ -0,0 +1,31 @@
+package ch.heigvd.dai.chill.domain.BoluxByte;
+
+import ch.heigvd.dai.chill.domain.Bartender;
+import ch.heigvd.dai.chill.protocol.OrderRequest;
+import ch.heigvd.dai.chill.protocol.OrderResponse;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CardinalTest {
+
+  @Test
+  void thePriceAndNameForCardinalShouldBeCorrect() {
+    Cardinal beer = new Cardinal();
+    assertEquals(beer.getName(), Cardinal.NAME);
+    assertEquals(beer.getPrice(), Cardinal.PRICE);
+  }
+
+  @Test
+  void aBartenderShouldAcceptAnOrderForPunkIPA() {
+    Bartender jane = new Bartender();
+    String productName = "ch.heigvd.dai.chill.domain.wasadigi.Cardinal";
+    OrderRequest request = new OrderRequest(3, productName);
+    OrderResponse response = jane.order(request);
+    BigDecimal expectedTotalPrice = Cardinal.PRICE.multiply(new BigDecimal(3));
+    assertEquals(expectedTotalPrice, response.getTotalPrice());
+  }
+
+}
